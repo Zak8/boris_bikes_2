e842624120a19c87d17bbc60d8d33b1021c7e3d4
Zak8 <zak00rashid@gmail.com>
2019-11-12 15:36:36 +0000

    linking the file

---
+++ lib/Docking_Station.rb
@@ -0,0 +1,15 @@
+class DockingStation
+
+
+
+
+
+
+
+
+
+
+end
+
+
+docking_station = DockingStation.new()
--- spec/Docking_Station_spec.rb
+++ spec/Docking_Station_spec.rb
@@ -1,5 +1,6 @@
+require 'Docking_Station'
 describe "DockingStation" do
-  # it "returns 'new' when DockingStation is initialized" do
-  #   expect(DockingStation.new).to eq "new"
-  # end
+#   it "returns 'DockingStation' when DockingStation is initialized" do
+#     expect(DockingStation.new).to eq 'DockingStation'
+#   end
 end
