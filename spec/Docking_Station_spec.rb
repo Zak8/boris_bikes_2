d62fc1181472c51192394bf544d461f837ac3082
Jade Beresford <jade_fletcher@hotmail.co.uk>
2019-11-12 16:21:57 +0000

    create method release_bike and pass test

--- spec/Docking_Station_spec.rb
+++ spec/Docking_Station_spec.rb
@@ -4,4 +4,7 @@ describe "DockingStation" do
   it "should exist" do
     expect(DockingStation.new).to be_instance_of DockingStation
   end
+
+  it { expect(DockingStation.new).to respond_to(:release_bike) }
+
 end
