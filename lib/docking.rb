9d7b32a28a47703900be2f5674e7b36136ad375a
Zak8 <zak00rashid@gmail.com>
2019-11-14 18:45:48 +0000

    docking.rb

--- lib/docking.rb
+++ lib/docking.rb
@@ -1,8 +1,9 @@
 class DockingStation
   attr_reader :bike
 
-  def release_bike
-    Bike.new
+  def release_bike(amount_of_bikes)
+    Bike.new if amount_of_bikes > 0
+    else
   end
 
   def dock(bike)
